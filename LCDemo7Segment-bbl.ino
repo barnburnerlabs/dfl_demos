22da5c33f78b81e4bdc4af68b7d8727d451f192c
barnburnerlabs <barnburnerlabs@gmail.com>
2017-03-30 13:48:30 -0400

    Initial Upload
    Demo files upload for MAX7219 LED display and TinkerKit board #1.

---
+++ LCDemo7Segment-bbl.ino
@@ -0,0 +1,144 @@
+// Mostly borrowed from LedControl Library available at
+// https://github.com/wayoda/LedControl
+
+//We always have to include the library
+#include "LedControl.h"
+
+/*
+ Now we need a LedControl to work with.
+ ***** These pin numbers will probably not work with your hardware *****
+ pin 12 is connected to the DataIn 
+ pin 11 is connected to the CLK 
+ pin 10 is connected to LOAD 
+ We have only a single MAX72XX.
+ */
+LedControl lc=LedControl(12,11,10,1);
+
+/* we always wait a bit between updates of the display */
+unsigned long delaytime=250;
+unsigned long delaytimeshort=100;
+unsigned long bigCounter=0;
+
+void setup() {
+  /*
+   The MAX72XX is in power-saving mode on startup,
+   we have to do a wakeup call
+   */
+  lc.shutdown(0,false);
+  /* Set the brightness to a medium values */
+  lc.setIntensity(0,8);
+  /* and clear the display */
+  lc.clearDisplay(0);
+}
+
+
+/*
+ This method will display the characters for the
+ word "Arduino" one after the other on digit 0. 
+ */
+void writeArduinoOn7Segment() {
+  lc.setChar(0,7,'a',false);
+  delay(delaytimeshort);
+  lc.setRow(0,6,0x05);
+  delay(delaytimeshort);
+  lc.setChar(0,5,'d',false);
+  delay(delaytimeshort);
+  lc.setRow(0,4,0x1c);
+  delay(delaytimeshort);
+  lc.setRow(0,3,B00010000);
+  delay(delaytimeshort);
+  lc.setRow(0,2,0x15);
+  delay(delaytimeshort);
+  lc.setRow(0,1,0x1D);
+  //delay(delaytime*1);
+  for(int i=7; i>=0;i--) {
+    lc.setChar(0,i,' ',false);
+    delay(delaytimeshort);
+  }
+  lc.clearDisplay(0);
+  delay(delaytimeshort);
+} 
+
+/*
+  This method will scroll all the hexa-decimal
+ numbers and letters on the display. You will need at least
+ four 7-Segment digits. otherwise it won't really look that good.
+ */
+void scrollDigits() {
+  for(int i=0;i<9;i++) {
+    lc.setDigit(0,7,i,false);
+    lc.setDigit(0,6,i+1,false);
+    lc.setDigit(0,5,i+2,false);
+    lc.setDigit(0,4,i+3,false);
+    lc.setDigit(0,3,i+4,false);
+    lc.setDigit(0,2,i+5,false);
+    lc.setDigit(0,1,i+6,false);
+    lc.setDigit(0,0,i+7,false);
+    delay(delaytime);
+  }
+  lc.clearDisplay(0);
+  delay(delaytime);
+}
+void scrollSingleDigit(byte dig, long dtime) {
+  for(int i=0; i<8; i++) {
+    lc.setDigit(0,i,dig,false);
+    delay(dtime);
+  }
+  for(int i=0; i<8; i++) {
+    lc.setChar(0,i,' ',false);
+    delay(dtime);
+  }
+  lc.clearDisplay(0);
+  for(int i=7; i>=0; i--) {
+    lc.setDigit(0,i,dig,false);
+    delay(dtime);
+  }
+  for(int i=7; i>=0; i--) {
+    lc.setChar(0,i,' ',false);
+    delay(dtime);
+  }
+  lc.clearDisplay(0);
+  //delay(dtime);
+}
+void bCounter(long incSpeed) {
+  long stepDown;
+  while(bigCounter < 100000000) {
+    lc.setDigit(0,0,bigCounter%10000000,false);
+    stepDown = bigCounter/10000000;
+    lc.setDigit(0,1,stepDown%1000000,false);
+    stepDown = stepDown/1000000;
+    lc.setDigit(0,2,stepDown%100000,false);
+    stepDown = stepDown/100000;
+    lc.setDigit(0,3,stepDown%10000,false);
+    stepDown = stepDown/10000;
+    lc.setDigit(0,4,stepDown%1000,false);
+    stepDown = stepDown/1000;
+    lc.setDigit(0,5,stepDown%100,false);
+    stepDown = stepDown/100;
+    lc.setDigit(0,6,stepDown%10,false);
+    stepDown = stepDown/10;
+    lc.setDigit(0,7,stepDown,false);
+    bigCounter++;
+    //delay(1);
+  }
+  bigCounter=0;
+}
+
+void loop() { 
+  writeArduinoOn7Segment();
+  writeArduinoOn7Segment();
+  writeArduinoOn7Segment();
+  writeArduinoOn7Segment();
+  //scrollDigits();
+  scrollSingleDigit(0,15);
+  scrollSingleDigit(1,15);
+  scrollSingleDigit(2,15);
+  scrollSingleDigit(3,15);
+  scrollSingleDigit(4,15);
+  scrollSingleDigit(5,15);
+  scrollSingleDigit(6,15);
+  scrollSingleDigit(7,15);
+  scrollSingleDigit(8,15);
+  scrollSingleDigit(9,15);
+ // bCounter(1);
+}
---
+++ TinkerkitDemo-bbl.ino
@@ -0,0 +1,62 @@
+/* https://github.com/TinkerKit
+ Read values from the T000090 LDR Analog Sensor connected to I0, 
+ then uses the result to set the brightness on a T010111 LED 
+ Module connected on O0. Also prints the values on the 
+ serial monitor.
+
+ created 29 Dec. 2008
+ Modified 4 Sep 2010
+ by Tom Igoe
+ modified 7 dec 2010
+ by Davide Gomba
+ modified on Ded 2011
+ by Federico Vanzati
+ modified Jun 2013
+ by Matteo Loglio<http://matlo.me>
+
+ This example code is in the public domain.
+ */
+
+#include <TinkerKit.h>
+
+TKLightSensor ldr(I1);	//create the "ldr" object on port I0
+TKPotentiometer pot(I0);
+TKButton button(I2);
+
+TKLed ledPot(O3);
+TKLed ledButton(O2);		//create the "led" object on port O0
+TKLed ledPhotoDark(O5);
+TKLed ledPhotoLight(O4);
+void setup() {
+  // nothing here.
+}
+
+void loop() {
+  // store the ldr values into a variable called brightnessVal
+  int brightnessVal = ldr.read();            
+  int brightnessPot = pot.read();
+  // set the led brightness
+  ledPot.brightness(brightnessPot);       
+  if(brightnessVal > 512) {
+    ledPhotoDark.off();
+    ledPhotoLight.on();
+  } else {
+    ledPhotoDark.on();
+    ledPhotoLight.off();
+  }
+  if(button.pressed()) {
+    ledButton.on();
+  }
+  if(button.held()) {
+    ledButton.off();
+    delay(50);
+    ledButton.on();
+    delay(50);
+  }
+  if(button.released()) {
+    ledButton.off();
+  }
+  
+  // wait 10 milliseconds before the next loop
+  delay(10);                    
+}
